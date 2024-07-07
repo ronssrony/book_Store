31d65e35ba128de41d8220b95d79053cc4054f8d
ronssrony <ronssrony@gmail.com>
2024-07-08 00:48:38 +0600

    Deopt Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+Product.delete_all
+# . . .
+Product.create!(title: 'Design and Build Great Web APIs',
+ description:   "<p>
+     <em>Robust, Reliable, and Resilient</em>
+     APIs are transforming the business world at an increasing pace. Gain
+     the essential skills needed to quickly design, build, and deploy
+     quality web APIs that are robust, reliable, and resilient. Go from
+     initial design through prototyping and implementation to deployment of
+     mission-critical APIs for your organization. Test, secure, and deploy
+     your API with confidence and avoid the “release into production”
+     panic. Tackle just about any API challenge with more than a dozen
+     open-source utilities and common programming patterns you can apply
+     right away.
+     </p>",
+ image_url: 'maapis.jpg',
+ price: 24.95 )
+# . . 
